7114f195cf2ebcc3c6bc7140c3c4b938e7fef872
James Lea <james.q.lea@gmail.com>
2023-05-31 15:08:20 -0600

    first commit

---
+++ PlaySound/PlaySoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaySoundApp.swift
+//  PlaySound
+//
+//  Created by James Lea on 5/31/23.
+//
+
+import SwiftUI
+
+@main
+struct PlaySoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
